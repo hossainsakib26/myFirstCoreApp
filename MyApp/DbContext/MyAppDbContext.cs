d128f6ffcb1e2c7df19594df92366a853f18c18b
Sazzad Hossain <hossain.sakib26@hotmail.com>
2021-07-10 11:54:53 +0600

    first commit of my core app

---
+++ MyApp/DbContext/MyAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyApp.Models.Main_Models;
+
+
+namespace MyApp.DbContext
+{
+    public class MyAppDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public MyAppDbContext(DbContextOptions<MyAppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<SubCategory> SubCategory { get; set; }
+    }
+}
